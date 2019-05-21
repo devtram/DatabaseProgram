1a4fa6f065adc8ffa8a383735ddc3d5f544fd627
MUHAMMAD TAYYAB RANA <titanscomputer@gmail.com>
2019-05-21 20:02:29 +0500

    Login with Router Done and Redux

--- App.js
+++ App.js
@@ -1,49 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component }  from "react";
+import { View, Text } from "react-native";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./src/reducers";
+import LoginForm from "./src/components/LoginForm";
+import firebase from "firebase";
+import ReduxThunk from "redux-thunk"; //i added this
+import Routes  from "./src/Router"
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+class App extends Component {
+state= { loggedIn: null };
+//initialized the firebase//
+componentWillMount() {
+    firebase.initializeApp({
+        apiKey: "AIzaSyBMMOJXeaxE-0cCdfblSSPDwTms1s0KGpM",
+        authDomain: "rnapp-auth-class-tayyab.firebaseapp.com",
+        databaseURL: "https://rnapp-auth-class-tayyab.firebaseio.com",
+        projectId: "rnapp-auth-class-tayyab",
+        storageBucket: "rnapp-auth-class-tayyab.appspot.com",
+        messagingSenderId: "586672115417",
+        appId: "1:586672115417:web:fe27fe289f851a84"
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+        
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+
+    });
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+ 
+render() {
+
+    const store = createStore(reducers, {},
+        applyMiddleware(ReduxThunk));
+
+    return (
+        <Provider store={store}>
+
+
+           {/* <LoginForm/> */
+           }
+           <Routes />
+       
+        
+        </Provider>
+    );
+}
+}
+export default App;
